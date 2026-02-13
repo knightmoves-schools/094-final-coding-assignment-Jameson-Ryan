6af7280a8f6ba28f08c0505c7c273a194091400a
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2026-02-13 18:53:32 +0000

    update index.html, index.js, & style.css

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Put your code here
+let total = 0;
+function addChair() {
+    total += 20;
+    document.getElementById("total").innerText = "Total: $" + total;
+}
+
+function addTable() {
+    total += 15;
+    document.getElementById("total").innerText = "Total: $" + total;
+}
+
+function addDesk() {
+    total += 30;
+    document.getElementById("total").innerText = "Total: $" + total;
+}
